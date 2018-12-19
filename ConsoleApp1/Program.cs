69f9d0d784b1e48c3501e4b1eb0ca4bd66695444
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2018-12-19 19:59:28 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int res = 1;
+        static object o = new object();
+
+        static void Main(string[] args)
+        {
+            
+            for (int i=1;i<10;i++)
+            {
+                Thread thread = new Thread(new ParameterizedThreadStart(Multip));
+                thread.Start(i);
+            }
+
+            Console.ReadLine();
+        }
+
+        static public void Multip(object a)
+        {
+            lock (o)
+            {
+                res = res * (int)a;
+                Console.WriteLine($"Результат:{res}, Множитель:{(int)a}");
+            }
+            
+        }
+    }
+}
